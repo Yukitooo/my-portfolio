ccd7cf1bbd8a633173599f480c7e2a2c4976673b
Yuki <127383277+Yukitooo@users.noreply.github.com>
2025-05-15 19:13:05 +0800

    Add files via upload
    Added details about me, fixed some links, and provided a footer.

---
+++ index.js
@@ -0,0 +1,31 @@
+
+  const modalPortfolio = document.getElementById("portfolio_modal");
+  const openBtnPortfolio = document.getElementById("portfolio_card");
+  const closeBtnPortfolio = document.querySelector(".close-btn");
+
+  const modalKatarina = document.getElementById("katarina_modal");
+  const openBtnKatarina = document.getElementById("katarina-card");
+  const closeBtnKatarina = document.querySelector(".close-btn-kat");
+
+  const modalDiscord = document.getElementById("discord_modal");
+  const openBtnDiscord = document.getElementById("discord-card");
+  const closeBtnDiscord = document.querySelector(".close-btn-discord");
+
+  openBtnPortfolio.onclick = () => modalPortfolio.style.display = "block";
+  closeBtnPortfolio.onclick = () => modalPortfolio.style.display = "none";
+  window.onclick = (e) => {
+    if (e.target === modalPortfolio) modalPortfolio.style.display = "none";
+  };
+
+  
+  openBtnKatarina.onclick = () => modalKatarina.style.display = "block";
+  closeBtnKatarina.onclick = () => modalKatarina.style.display = "none";
+  window.onclick = (e) => {
+    if (e.target === modalKatarina) modalKatarina.style.display = "none";
+  };
+
+  openBtnDiscord.onclick = () => modalDiscord.style.display = "block";
+  closeBtnDiscord.onclick = () => modalDiscord.style.display = "none";
+  window.onclick = (e) => {
+    if (e.target === modalDiscord) modalDiscord.style.display = "none";
+  };
