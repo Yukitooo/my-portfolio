8c3633cf0f202807bf4a7ac917fce4f2eb560ca6
Yuki <127383277+Yukitooo@users.noreply.github.com>
2025-05-28 12:20:41 +0800

    Add files via upload
    Added the pokemon project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
   const modalPortfolio = document.getElementById("portfolio_modal");
   const openBtnPortfolio = document.getElementById("portfolio_card");
   const closeBtnPortfolio = document.querySelector(".close-btn");
+  
 
   const modalKatarina = document.getElementById("katarina_modal");
   const openBtnKatarina = document.getElementById("katarina-card");
@@ -10,6 +11,10 @@
   const modalDiscord = document.getElementById("discord_modal");
   const openBtnDiscord = document.getElementById("discord-card");
   const closeBtnDiscord = document.querySelector(".close-btn-discord");
+
+   const modalPokemon = document.getElementById("pokemon_modal");
+  const openBtnPokemon= document.getElementById("pokemon-card");
+  const closeBtnPokemon = document.querySelector(".close-btn-pokemon");
 
   openBtnPortfolio.onclick = () => modalPortfolio.style.display = "block";
   closeBtnPortfolio.onclick = () => modalPortfolio.style.display = "none";
@@ -28,4 +33,10 @@
   closeBtnDiscord.onclick = () => modalDiscord.style.display = "none";
   window.onclick = (e) => {
     if (e.target === modalDiscord) modalDiscord.style.display = "none";
+  };
+
+  openBtnPokemon.onclick = () => modalPokemon.style.display = "block";
+  closeBtnPokemon.onclick = () => modalPokemon.style.display = "none";
+  window.onclick = (e) => {
+    if (e.target === modalPokemon) modalPokemon.style.display = "none";
   };
